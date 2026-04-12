b92a3ed4cd612383b63fd4702eea0a258a70357e
KryptonianSuperman <KryptonianSuperman@users.noreply.github.com>
2026-04-12 10:32:31 +0530

    Add: longest-repeating-substring-with-replacement - submission-5

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-5.js
@@ -0,0 +1,35 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {number} k
+     * @return {number}
+     */
+    characterReplacement(s, k) {
+        let maxLength = 0
+        let lp = 0
+        let rp = 0
+        const stringFrequency = new Map()
+        for(;rp<s.length; rp++){
+            stringFrequency.set(s[rp], (stringFrequency.get(s[rp]) || 0) +1)
+            let mostFrequentItemSize = Math.max(...stringFrequency.values())
+            // console.log("mostFrequentSize", mostFrequentItemSize)
+            let windowSize = rp-lp +1
+            // console.log("windowSize", windowSize)
+            if(windowSize - mostFrequentItemSize > k){
+                // console.log("weregettignherE", lp, s[lp])
+                const lpFreq = stringFrequency.get(s[lp])
+                if( lpFreq > 1){
+                    stringFrequency.set(s[lp], lpFreq-1)
+                } else{
+                    stringFrequency.delete(s[lp])
+                }
+                lp++
+                windowSize--
+            }
+            // console.log("maxLength", maxLength, "windowSize", windowSize)
+            maxLength = Math.max(maxLength, windowSize)
+            // console.log("maxLength", maxLength)
+        }
+        return maxLength
+    }
+}
