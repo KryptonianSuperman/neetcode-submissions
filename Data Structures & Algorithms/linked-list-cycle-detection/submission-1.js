62d01f666f32a5afb23b2742a1df96725743c931
KryptonianSuperman <KryptonianSuperman@users.noreply.github.com>
2026-04-12 12:31:07 +0530

    Add: linked-list-cycle-detection - submission-1

---
+++ Data Structures & Algorithms/linked-list-cycle-detection/submission-1.js
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     constructor(val = 0, next = null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {ListNode} head
+     * @return {boolean}
+     */
+
+    hasCycle(head) {
+        const list = new Set()
+        function childNodeValue(node, set){
+            if(node?.next){
+                // console.log("here", node, set)
+                if(set.has(node.next.val)){
+                    // console.log("true")
+                    return true
+                }
+                set.add(node.next.val)
+                return childNodeValue(node.next, set)
+            }
+            return false
+        }
+        return childNodeValue(head, list)
+
+        // return false
+    }
+}
